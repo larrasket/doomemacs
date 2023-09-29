915786295e93553b0102d41795326c6799925df0
Salih Muhammed <lr0@gmx.com>
2023-09-30 02:19:24 +0300

    bump :lang ess

--- modules/lang/ess/packages.el
+++ modules/lang/ess/packages.el
@@ -1,10 +1,10 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/ess/packages.el
 
-(package! ess :pin "8b4664e4a76a573b76ab3cf7a467d5e20fd5a6de")
+(package! ess :pin "d8914196ceb2061d850cc899aed79342519972ff")
 (package! ess-R-data-view :pin "d6e98d3ae1e2a2ea39a56eebcdb73e99d29562e9")
-(package! polymode :pin "15b6c1e94a450a65a0e32096855c31f4390a3963")
-(package! poly-R :pin "e4a39caaf48e1c2e5afab3865644267b10610537")
+(package! polymode :pin "ca060e081a1f849a880732670dc15370ac987b89")
+(package! poly-R :pin "8024e852cfca642dea2045a41b2033baa2f1f9a5")
 
 (when (modulep! +stan)
   (package! stan-mode :pin "150bbbe5fd3ad2b5a3dbfba9d291e66eeea1a581")
